19a9eac6f222ac12bf693b2a51d6423359937f94
Gurj <118504032+Gurj-YT@users.noreply.github.com>
2022-11-30 13:52:01 -0800

    Updated sequence timing

--- scripts/cmd-seq.js
+++ scripts/cmd-seq.js
@@ -47,6 +47,9 @@ function updateResult(){
     // Updates the command for starting the sequence
     $(`#sequence_starter`).html(`scoreboard players add value ${cmd_objective} 0`);
 
+    // Updates the command for making the sequence progress
+    html += `execute if score value ${cmd_objective} matches 0.. run scoreboard players add value ${cmd_objective} 1\n`;
+
     // For each form:
     $(`form`).each(function(){
         // Fetches all user input
@@ -59,11 +62,11 @@ function updateResult(){
         // If the current command is set to run once
         if(pulse == "impulse"){
             // Runs the command only once
-            realTicks = `${initialTicks}`;
+            realTicks = `${initialTicks + 1}`;
         // If the current command is set to repeat for the whole duration
         }else{
             // Runs the command for the whole duration
-            realTicks = `${initialTicks} .. ${totalTicks}`;
+            realTicks = `${initialTicks + 1} .. ${totalTicks}`;
         }
         // The 
         initialTicks += Math.round(duration * 20);
